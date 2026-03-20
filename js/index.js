2525ee435b589656f5a23b7e68519ff479461238
Ronikok <ronikokkonen@gmail.com>
2026-03-20 23:13:41 +0200

    possibly frontend deployment

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
-const BACKEND_ROOT_URL = 'http://localhost:3001'
+//const BACKEND_ROOT_URL = 'http://localhost:3001'
+const BACKEND_ROOT_URL = 'https://todo-server-7w47.onrender.com'
 import {Todos} from './class/Todos.js'
 
 const todos = new Todos(BACKEND_ROOT_URL)
